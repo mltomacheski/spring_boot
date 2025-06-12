20c1c924f0b9fb29f4fd7decdfde2ea22dca2c72
mltomacheski <tomacheski.lara@escola.pr.gov.br>
2025-06-12 11:23:21 -0300

    primeiro commit

---
+++ src/main/java/com/coelho/coelinho/CoelinhoApplication.java
@@ -0,0 +1,13 @@
+package com.coelho.coelinho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoelinhoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoelinhoApplication.class, args);
+	}
+
+}
